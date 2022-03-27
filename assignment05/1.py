88a7ef28f0066190d0f791c301f0d511f81ea0d2
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-27 10:14:28 -0700

    Assignment 5 update

---
+++ assignment05/1.py
@@ -0,0 +1,17 @@
+# Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+# Of course we do not want to sprinkle print statements, so let us generate them.
+
+def fizz_buzz(n: int)-> list(str):
+    #Write your code here
+    return 
+
+# Write a function that Converts vowels into upper case character and consonants into lower case character in a given string .
+def vowel_to_upper_cons_to_lower(string: str)-> str:
+    #Write your code here
+    return 
+
+# Write a function that classifies given list of numbers into Even and Odd Sublists.
+def split_Even_Odd_Sublist(num_array): 
+    #Write your code here
+    return 
