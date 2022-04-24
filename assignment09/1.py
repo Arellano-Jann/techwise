d5c86aedb413f992412b74d62763a5916685de4d
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-24 00:42:27 -0700

    Finished 9

--- assignment09/1.py
+++ assignment09/1.py
@@ -26,3 +26,63 @@ def rus_mult_print(a: int, b: int) -> None:
     return b + rus_mult_print(a // 2, b*2)
 
 print(rus_mult_print(13, 17))
+
+
+def line(num_stars: int) -> str: # returns a line of n *'s
+    return "*" + (num_stars * " *") + ""
+
+def make_triangle(n): # returns list of stars
+    triangle = []
+    for i in range(n):
+        triangle.append(line(i))
+    return triangle
+
+# How to produce an upside down triangle?
+
+def upside_down_triangle(length: int):
+    #write your code here
+    triangle = make_triangle(length)
+    spaced_triangle = []
+    for i in triangle[::-1]:
+        spaced_triangle.append(i.center(length*10))
+    return "\n".join(spaced_triangle)
+
+print(upside_down_triangle(7))
+
+# How to produce a right_angled triangle?
+
+def right_angle(length: int):
+    #write your code here
+    triangle = make_triangle(length)
+    spaced_triangle = []
+    for i in triangle:
+        spaced_triangle.append(i.center(0))
+    return "\n".join(spaced_triangle)
+
+print(right_angle(7))
+
+# How to produce a right_angled triangle, facing the other way?
+
+def right_angle_opp(length: int):
+    #write your code here
+    triangle = make_triangle(length)
+    spaced_triangle = []
+    for i in triangle:
+        spaced_triangle.append('%50s' % i)
+    return "\n".join(spaced_triangle)
+
+print(right_angle_opp(7))
+
+# How to produce a diamond?
+
+def diamond(length: int):
+    #write your code here
+    triangle = make_triangle(length)
+    spaced_triangle = []
+    for i in triangle:
+        spaced_triangle.append(i.center(length*10))
+    for i in triangle[length-2::-1]:
+        spaced_triangle.append(i.center(length*10))
+    return "\n".join(spaced_triangle)
+
+print(diamond(13))
