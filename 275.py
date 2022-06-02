1ad48af827e7f8b4ebf218aa0d9554e21141d240
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-06-02 13:01:56 -0700

    finished commenting happy num

--- 275.py
+++ 275.py
@@ -3,7 +3,7 @@
 def num2list(num):
     return [x for x in str(num)]
     
-def is_happy_helper(n):
+def is_happy_helper(n): # returns the sum of all digits to the second power
     lst = num2list(n)
     num = 0
     for val in lst:
@@ -13,10 +13,10 @@ def is_happy_helper(n):
 
 def is_happy(n: int) -> bool:
     count = 0
-    while n != 1 and count <= 990:
+    while n != 1 and count <= 990: # 990 recursion limit
         count += 1
         n = is_happy_helper(n)
-    if count >= 989:
+    if count >= 989: # checks if recursion limit is hit
         return False
     return True
 
@@ -24,3 +24,5 @@ def is_happy(n: int) -> bool:
 
 print(is_happy(19)) # true
 print(is_happy(2)) # false 
+
+
