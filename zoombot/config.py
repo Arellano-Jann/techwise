7572af553f8f48a96f025d6ba3fca45deabed9d5
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-06-19 23:54:36 -0700

    Fixed

--- zoombot/config.py
+++ zoombot/config.py
@@ -1,10 +1,6 @@
-# minute = 0 # 00 PST
 hour = "18:00" # 18 # 6:00 PM PST
-# days = [0, 2, 4] # Mon, Wed, Fri
-# tabs = [9, 11] # this is nothing anymore.
 time_in_meeting = 60*1.75*60 # 60 min * 1.75 hours * 60 seconds
-# meeting_link = "https://tmcc-edu.zoom.us/j/94817742847?pwd=MEhZN25HdWYxb2RhZHZBSTNpaTQ1UT09" # test link
-meeting_link = "https://learn.techwise.talentsprint.com/extras/join_zoom_meeting?meeting_id=92084655704"
-user_id = "jannrellano@nevada.unr.edu"
-password = "7755014845"
-nameTag = "Jann A"
+meeting_link = "https://zoomlink.here"
+user_id = "johndoe@gmail.com"
+password = "7541838494"
+nameTag = "John Doe"
